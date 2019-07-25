b0ce22d5d9b927f2309d2a6d511718d70fdbc473
jisongzhou <568777035@qq.com>
2019-07-25 11:00:41 +0800

    优化代码

--- src/main/java/com/feiyue/example/spring/cloud/HelloWorldApplication.java
+++ src/main/java/com/feiyue/example/spring/cloud/HelloWorldApplication.java
@@ -3,10 +3,7 @@ package com.feiyue.example.spring.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -18,7 +15,7 @@ public class HelloWorldApplication {
 
 	@RestController
 	class EchoController {
-		@RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
+		@GetMapping(value = "/echo/{string}")
 		public String echo(@PathVariable String string) {
 			return "Hello Nacos Discovery " + string;
 		}
