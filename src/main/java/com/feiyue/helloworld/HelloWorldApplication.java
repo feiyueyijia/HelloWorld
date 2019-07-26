c895878e80d93249656d35bd0dd92e6981eeb963
jisongzhou <568777035@qq.com>
2019-07-26 16:54:20 +0800

    代码调整

--- src/main/java/com/feiyue/helloworld/HelloWorldApplication.java
+++ src/main/java/com/feiyue/helloworld/HelloWorldApplication.java
@@ -1,4 +1,4 @@
-package com.feiyue.example.spring.cloud;
+package com.feiyue.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
