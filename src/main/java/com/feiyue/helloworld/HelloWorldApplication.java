e705321c08372288693d7a1624b1d82d29eb564b
jisongZhou <568777035@qq.com>
2022-04-08 16:42:50 +0800

    更新修复依赖

--- src/main/java/com/feiyue/helloworld/HelloWorldApplication.java
+++ src/main/java/com/feiyue/helloworld/HelloWorldApplication.java
@@ -2,11 +2,9 @@ package com.feiyue.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
@@ -17,7 +15,7 @@ public class HelloWorldApplication {
 	class EchoController {
 		@GetMapping(value = "/echo/{string}")
 		public String echo(@PathVariable String string) {
-			return "Hello Nacos Discovery " + string;
+			return "Hello Discovery Info " + string;
 		}
 	}
 }
